420db6bbdd0fb1c4cb12ff2a44cbfc5783c3545d
Tackyjarl <wallaceandrew990@gmail.com>
2025-01-05 17:40:22 -0500

    update cors

--- app.js
+++ app.js
@@ -12,15 +12,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 const app = express();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: [
-      "https://www.tackyjarl.crabdance.com",
-      "https://api.tackyjarl.crabdance.com",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  })
-);
+app.use(cors());
 
 app.use(requestLogger);
 app.use("/", routes);
